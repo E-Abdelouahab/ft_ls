96003ed5c0d9525e6b6628b122f903b49f30d4af
Ismail Elmoudni <ielmoudn@e2r2p15.1337.ma>
2019-07-21 19:36:33 +0100

    partially normed 2 - functions len

--- src/read_all.c
+++ src/read_all.c
@@ -6,15 +6,48 @@
 /*   By: ielmoudn <ielmoudn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 16:18:43 by ielmoudn          #+#    #+#             */
-/*   Updated: 2019/07/21 00:54:27 by ielmoudn         ###   ########.fr       */
+/*   Updated: 2019/07/21 19:35:31 by ielmoudn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_ls.h"
 
-void handle_error2(char *name)
+void	read_all_helper1(struct dirent *dp, t_info **info,
+	t_node **all, t_node **dirs)
 {
-	ft_printf("ft_ls: %s: %s\n\n", name, strerror(errno));
+	normal_fill(info, dp);
+	(*info)->insert_func((char*)(all), (char*)info);
+	((*info)->list_len) += 1;
+	if (is_dir(dp->d_type) && ft_strcmp(dp->d_name, ".") != 0
+		&& ft_strcmp(dp->d_name, "..") != 0)
+		(*info)->insert_func((char*)(dirs), (char*)info);
+}
+
+void	read_all_helper2(t_node *all, t_node **dirs, t_node **head,
+	t_info **info)
+{
+	t_node *tbf;
+
+	tbf = all;
+	if (all)
+		(*info)->print_func(all, *info);
+	else
+		ft_printf("\n");
+	if (tbf != NULL)
+		free_nodes(tbf);
+	tbf = *dirs;
+	if ((*info)->flags & FLAG_RCAP)
+	{
+		while (*dirs)
+		{
+			(*info)->path_tbi = (*head)->path;
+			(*info)->name_tbi = (*dirs)->name;
+			read_all(dirs, info, (*info)->counter + 1);
+			*dirs = (*dirs)->next;
+		}
+	}
+	if	(tbf != NULL)
+		free_nodes(tbf);
 }
 
 void	read_all(t_node **head, t_info **info, int tracker)
@@ -23,44 +56,25 @@ void	read_all(t_node **head, t_info **info, int tracker)
 	struct dirent	*dp;
 	t_node			*dirs;
 	t_node			*all;
-	t_node			*copy;
 
 	dirs = NULL;
 	all = NULL;
 	dir = opendir((*head)->path);
+	if (!((*head)->perm & S_IXUSR))
+		return;
 	if (!dir)
 	{
-		handle_error2((*head)->name);
-		return;
+		handle_error((*head)->name);
+		return ;
 	}
-	if (tracker)
+	if (tracker != 0)
 		ft_printf("%s:\n", (*head)->path);
 	(*info)->path_tbi = (*head)->path;
 	(*info)->list_len = 0;
 	(*info)->max_len = 0;
 	while ((dp = readdir(dir)) != NULL)
-	{
-			normal_fill(info, dp);
-			(*info)->insert_func((char*)(&all), (char*)info);
-			((*info)->list_len)++;
-			if (is_dir(dp->d_type) && ft_strcmp(dp->d_name, ".") != 0
-				&& ft_strcmp(dp->d_name, "..") != 0)
-				(*info)->insert_func((char*)(&dirs), (char*)info);
-	}
-	if (all)
-		(*info)->print_func(all, *info);
-	else
-		ft_printf("\n");
-	if ((*info)->flags & FLAG_RCAP)
-	{
-		while (dirs)
-		{
-				(*info)->path_tbi = (*head)->path;
-				(*info)->name_tbi = dirs->name;
-				copy = new_lnode(*info);
-				read_all(&copy, info, tracker + 1);
-				dirs = dirs->next;
-		}
-	}
+		read_all_helper1(dp, info, &all, &dirs);
+	(*info)->counter = tracker;
+	read_all_helper2(all, &dirs, head, info);
 	closedir(dir);
 }
--- src/read_content.c
+++ src/read_content.c
@@ -6,11 +6,52 @@
 /*   By: ielmoudn <ielmoudn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 01:11:00 by ielmoudn          #+#    #+#             */
-/*   Updated: 2019/07/21 00:54:27 by ielmoudn         ###   ########.fr       */
+/*   Updated: 2019/07/21 19:30:59 by ielmoudn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_ls.h"
+
+void	read_content_helper1(struct dirent *dp, t_info **info,
+	t_node **all, t_node **dirs)
+{
+	if (!(!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..")
+		|| is_hidden(dp->d_name)))
+	{
+		normal_fill(info, dp);
+		(*info)->insert_func((char*)(all), (char*)info);
+		((*info)->list_len) += 1;
+		if (is_dir(dp->d_type))
+			(*info)->insert_func((char*)(dirs), (char*)info);
+	}
+}
+
+void	read_content_helper2(t_node *all, t_node **dirs, t_node **head,
+	t_info **info)
+{
+	t_node *tbf;
+
+	tbf = all;
+	if (all)
+		(*info)->print_func(all, *info);
+	else
+		ft_printf("\n");
+	if (tbf != NULL)
+		free_nodes(tbf);
+	tbf = *dirs;
+	if ((*info)->flags & FLAG_RCAP)
+	{
+		while (*dirs)
+		{
+			(*info)->path_tbi = (*head)->path;
+			(*info)->name_tbi = (*dirs)->name;
+			read_content(dirs, info, (*info)->counter + 1);
+			*dirs = (*dirs)->next;
+		}
+	}
+	if	(tbf != NULL)
+		free_nodes(tbf);
+}
 
 void	read_content(t_node **head, t_info **info, int tracker)
 {
@@ -18,18 +59,16 @@ void	read_content(t_node **head, t_info **info, int tracker)
 	struct dirent	*dp;
 	t_node			*dirs;
 	t_node			*all;
-	t_node			*tbf;
 
 	dirs = NULL;
 	all = NULL;
-	tbf = NULL;
 	dir = opendir((*head)->path);
 	if (!((*head)->perm & S_IXUSR))
 		return;
 	if (!dir)
 	{
 		handle_error((*head)->name);
-		return;
+		return ;
 	}
 	if (tracker != 0)
 		ft_printf("%s:\n", (*head)->path);
@@ -37,37 +76,8 @@ void	read_content(t_node **head, t_info **info, int tracker)
 	(*info)->list_len = 0;
 	(*info)->max_len = 0;
 	while ((dp = readdir(dir)) != NULL)
-	{
-		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") || is_hidden(dp->d_name))
-			continue;
-		else
-		{
-			normal_fill(info, dp);
-			(*info)->insert_func((char*)(&all), (char*)info);
-			((*info)->list_len) += 1;
-			if (is_dir(dp->d_type))
-				(*info)->insert_func((char*)(&dirs), (char*)info);
-		}
-	}
-	tbf = all;
-	if (all)
-		(*info)->print_func(all, *info);
-	else
-		ft_printf("\n");
-	if (tbf != NULL)
-	free_nodes(tbf);
-	tbf = dirs;
-	if ((*info)->flags & FLAG_RCAP)
-	{
-		while (dirs)
-		{
-			(*info)->path_tbi = (*head)->path;
-			(*info)->name_tbi = dirs->name;
-			read_content(&dirs, info, tracker + 1);
-			dirs = dirs->next;
-		}
-	}
-	if(tbf != NULL)
-		free_nodes(tbf);
+		read_content_helper1(dp, info, &all, &dirs);
+	(*info)->counter = tracker;
+	read_content_helper2(all, &dirs, head, info);
 	closedir(dir);
 }
